3908ce8e4f3bfe4c67d8566cc262e2c21458cc05
Raph Suttiyotin <rsuttiyotin@seek.com.au>
2023-04-21 15:51:17 +1000

    Connect graphql API to backend API

--- graphql-api/dist/index.js
+++ graphql-api/dist/index.js
@@ -1,22 +1,21 @@
 import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
-// A schema is a collection of type definitions (hence "typeDefs")
-// that together define the "shape" of queries that are executed against
-// your data.
+import fetch from "node-fetch";
 const typeDefs = `#graphql
-# Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
-
-# This "Book" type defines the queryable fields for every book in our data source.
 type Book {
     title: String
     author: String
 }
 
-# The "Query" type is special: it lists all of the available queries that
-# clients can execute, along with the return type for each. In this
-# case, the "books" query returns an array of zero or more Books (defined above).
+type Villager {
+    id: String
+    name: String
+    image: String
+}
+
 type Query {
     books: [Book]
+    villagers: [Villager]
 }
 `;
 const books = [
@@ -34,6 +33,10 @@ const books = [
 const resolvers = {
     Query: {
         books: () => books,
+        villagers: async () => {
+            const response = await fetch('http://stardew-valley-api:28848/villagers');
+            return response.json();
+        },
     },
 };
 // The ApolloServer constructor requires two parameters: your schema
--- graphql-api/src/index.ts
+++ graphql-api/src/index.ts
@@ -1,23 +1,23 @@
 import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
+import fetch from "node-fetch";
 
-// A schema is a collection of type definitions (hence "typeDefs")
-// that together define the "shape" of queries that are executed against
-// your data.
+
 const typeDefs = `#graphql
-# Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
-
-# This "Book" type defines the queryable fields for every book in our data source.
 type Book {
     title: String
     author: String
 }
 
-# The "Query" type is special: it lists all of the available queries that
-# clients can execute, along with the return type for each. In this
-# case, the "books" query returns an array of zero or more Books (defined above).
+type Villager {
+    id: String
+    name: String
+    image: String
+}
+
 type Query {
     books: [Book]
+    villagers: [Villager]
 }
 `;
 
@@ -37,6 +37,10 @@ const books = [
 const resolvers = {
     Query: {
         books: () => books,
+        villagers: async () => {
+            const response = await fetch('http://stardew-valley-api:28848/villagers');
+            return response.json();
+        },
     },
 };
 
